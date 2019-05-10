3c79cae1b8a539896e2196353a1642ce04371c6b
andersoncscz <andersoncscz@hotmail.com>
2019-05-10 12:09:50 -0300

    refactoring hypermedia + CORS

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     globals: {
-        testURL: `http://localhost:5001`,
+        testURL: `http://localhost:3001`,
         auth: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhbmRlcnNvbmNzY3pAaG90bWFpbC5jb20iLCJuYW1lIjoiQW5kZXJzb24gQ3J1eiJ9.3Kftr_v871RX1vC2kz6n_LQpnL91q7s-odpJlo0ihPs"
     }
 };
